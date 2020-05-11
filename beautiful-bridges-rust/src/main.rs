6d89d99c90da90c3bb0ccb4be61370a49f69eb9d
jdmejiav <jdmejiav@eafit.edu.co>
2020-05-11 08:03:52 -0500

    Rust modificado

--- beautiful-bridges-rust/src/main.rs
+++ beautiful-bridges-rust/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 	io::stdin().read_line(&mut entrada).unwrap();		
 	let _first_line:Vec<&str> = entrada.split(" ").collect();
 		
-	let _num_iter:u64 = match _first_line[0].trim().parse(){
+	let _num_iter:usize = match _first_line[0].trim().parse(){
 		Ok(num)=>{
 			if num < 2 {
 				println!("Impossible, first number should be the number of entries, and it should be a number 2≤n");
@@ -22,7 +22,7 @@ fn main() {
 		},
 
 	};
-	let _h:u64 = match _first_line[1].trim().parse(){
+	let _h:usize = match _first_line[1].trim().parse(){
 		Ok(num)=>{
 			if num < 1 {
 				println!("Impossible, second number of the first line, should be the heigh of the bridge an it should be 1≤h");
@@ -38,7 +38,7 @@ fn main() {
 
 
 	}; 
-	let _a:u64 = match _first_line[2].trim().parse(){
+	let _a:usize = match _first_line[2].trim().parse(){
 		Ok(num)=>{
 			if num <1{
 				println!("Impossible, third number of the first line, means the pillars cost");
@@ -51,7 +51,7 @@ fn main() {
 			process::exit(0x0100)
 		}
 	};
-	let _b:u64 = match _first_line[3].trim().parse(){
+	let _b:usize = match _first_line[3].trim().parse(){
 		Ok(num)=>{
 			if num<1{
 				println!("Imposiible, fourth number of the first line, means the arch cost");
@@ -65,13 +65,12 @@ fn main() {
 		}
 
 	};
-	let _coordinate_x: Vec<u64>=Vec::new();
-	let _coordinate_y: Vec<u64>=Vec::new();	
+	let mut _coordinate_x:Vec<usize>=Vec::with_capacity(_num_iter);
+	let mut _coordinate_y:Vec<usize>=Vec::with_capacity(_num_iter);	
 	let mut temp:String=String::new();
-	let vec_temp:Vec<&str>;
 	for i in 0.._num_iter-1 {
 		io::stdin().read_line(&mut temp).unwrap();
-		vec_temp=temp.split(" ").collect();
+		let vec_temp:Vec<&str>=temp.split(" ").collect();
 		_coordinate_x[i] = match vec_temp[0].trim().parse(){
 			Ok(num) => num,
 			Err(_)=>{
@@ -79,7 +78,23 @@ fn main() {
 				process::exit(0x0100)
 			}
 			
-		}  
+		};
+		_coordinate_y[i] = match vec_temp[0].trim().parse(){
+			Ok(num)=>{
+				if num>_h{
+					println!("Impossible, y coordinate can't be higher than high");
+					process::exit(0x0100)
+				}
+				num
+			},
+			Ok(_)=>{
+				println!("Impossible, coordinates should be numbersm, and it should be higher than 0");
+					process::exit(0x0100)
+			}
+			
+
+		}
+		
 	}
 
 }
